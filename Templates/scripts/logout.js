a9f67fa52bcc3dc07d2404e4e05dac87696e03a6
Vid11 <33712810+Vid11@users.noreply.github.com>
2024-03-18 03:37:29 +0530

    Add files via upload

---
+++ Templates/scripts/logout.js
@@ -0,0 +1,6 @@
+<script>
+    function logout() {
+        // Perform logout actions here, such as redirecting to a logout page
+        window.redirect.href = "login.html" // Replace "/logout" with the actual logout URL
+    }
+</script>
